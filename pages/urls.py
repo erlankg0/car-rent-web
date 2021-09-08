7f0aeb53f76a3e192548d81b6b8c33492d92d24a
Erlan <era.ab.2001@gmail.com>
2021-09-08 15:28:09 +0300

    add pages

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import home
+from .views import home, about, contact, service, cars
 
 urlpatterns = [
     path('', home, name='home'),
+    path('carhouse/about/', about, name='about'),
+    path('carhouse/contact/', contact, name='contact'),
+    path('carhouse/cars/', cars, name='cars'),
+    path('carhouse/service/', service, name='service'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -3,4 +3,20 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'pages/index.html')
+
+
 # Create your views here.
+def cars(request):
+    return render(request, 'pages/cars.html')
+
+
+def about(request):
+    return render(request, 'pages/about.html')
+
+
+def contact(request):
+    return render(request, 'pages/contact.html')
+
+
+def service(request):
+    return render(request, 'pages/service.html')
