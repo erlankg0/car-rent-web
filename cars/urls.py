9b797f810f42f4edffe719fd5a43703639b78c83
Erlan <era.ab.2001@gmail.com>
2021-09-11 18:39:41 +0300

    Car detail

--- cars/urls.py
+++ cars/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from cars.views import cars
+from cars.views import car_detail, cars
 
 urlpatterns = [
     path('', cars, name='cars'),
+    path('car_detail/<int:pk>/', car_detail, name='car_detail'),
 ]
--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from cars.models import Car
 
 
 def cars(request):
     return render(request, 'pages/cars.html')
+
+
 # Create your views here.
+
+def car_detail(request, pk):
+    car = get_object_or_404(Car, pk=pk)
+    context = {
+        'car': car
+    }
+    return render(request, 'cars/car_detail.html', context=context)
