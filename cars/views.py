6b9d7439707d2136e8dabba957b2ec19b99e7542
Erlan <era.ab.2001@gmail.com>
2021-09-13 21:18:58 +0300

    Search first completed

--- cars/views.py
+++ cars/views.py
@@ -5,7 +5,7 @@ from django.core.paginator import EmptyPage, Paginator, PageNotAnInteger
 
 def cars(request):
     all_cars = Car.objects.all()
-    paginator = Paginator(all_cars, 2)
+    paginator = Paginator(all_cars, 6)
     page = request.GET.get('page')
     paged_cars = paginator.get_page(page)
     context = {
@@ -23,3 +23,15 @@ def car_detail(request, pk):
     }
 
     return render(request, 'cars/car_detail.html', context=context)
+
+
+def search(request):
+    search_cars = Car.objects.all()
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            search_cars = search_cars.filter(car_title__icontains=keyword)
+    context = {
+        'search_cars': search_cars
+    }
+    return render(request, 'cars/search.html', context=context)
